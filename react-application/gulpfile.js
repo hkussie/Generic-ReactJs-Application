8ffe55e01ab814f0a422e3d2551cfc00f1dc60d6
hkussie <hkussie@gmail.com>
2017-12-29 06:05:47 -0500

    Clean gulpfile

--- react-application/gulpfile.js
+++ react-application/gulpfile.js
@@ -3,11 +3,6 @@
 //No need to install gulp modules individually
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-//var sourcemap = require('gulp-sourcemaps');
-//var minifyCSS = require('gulp-clean-css');
-//var uglify = require('gulp-uglify');
-//var rename = require('gulp-rename');
-//var changed = require('gulp-changed');
 
 ///////////////
 // - SCS/CSS
@@ -25,31 +20,6 @@ gulp.task('sass', function() {
 gulp.task('watch_scss', function() {
   gulp.watch(SCSS_SRC, ['sass']);
 });
-
-//Compile CSS Code
-//1. Call Gulp Source to get scss Code
-//2. Call pipe method to process scss code into css
-//3. Output processed code into SCSS_DEST file
-
-/*
-gulp.task('compile_scss', function(){
-
-  gulp.src(SCSS_SRC)
-  .pipe(sass().on('error', sass.logError))
-  .pipe(minifyCSS())
-  .pipe(rename({ suffix: '.min' }))
-  .pipe(change(SCS_DEST))
-  .pipe(gulp.dest(SCSS_DEST));
-
-});
-*/
-//Detect changes in SCSS
-
-/*
-gulp.task('watch_scss', function(){
-  gulp.watch(SCSS_SRC, ['compile_scss'])
-});
-*/
 
 //Run tasks
 gulp.task('default', ['watch_scss']);
