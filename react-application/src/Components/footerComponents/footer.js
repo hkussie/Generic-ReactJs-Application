f4fcb71e1d70ee6f4b97fe530707bfbd988b6a55
hkussie <hkussie@gmail.com>
2018-01-04 18:44:12 -0500

    Add footer components to the project

--- react-application/src/Components/footerComponents/footer.js
+++ react-application/src/Components/footerComponents/footer.js
@@ -1,1 +1,15 @@
-var footer = 'footer';
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+
+      My Footer
+
+      </footer>
+    );
+  }
+}
+
+export default Footer;
