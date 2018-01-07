fb54f0c59281654ac5b2952878d354132915a8c3
hkussie <hkussie@gmail.com>
2018-01-07 03:29:37 -0500

    Add Modifications to App.js

--- react-application/src/App.js
+++ react-application/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './Assets/css/default.css';
 import Header from './Components/headerComponents/header';
 import Footer from './Components/footerComponents/footer';
+import Homepage from './Components/pages/homePage';
 
 class App extends Component {
   render() {
@@ -9,6 +10,8 @@ class App extends Component {
       <div className="App">
 
       <Header />
+
+      <Homepage />
 
       <Footer />
 
